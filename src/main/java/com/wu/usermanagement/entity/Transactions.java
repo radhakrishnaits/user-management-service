f93938045722b618a7eb54125b4431eeab43605a
Radha Krishna Betha <rbetha@altimetrik.com>
2023-08-22 19:29:21 +0530

    changes files

--- src/main/java/com/wu/usermanagement/entity/Transactions.java
+++ src/main/java/com/wu/usermanagement/entity/Transactions.java
@@ -1,6 +1,5 @@
 package com.wu.usermanagement.entity;
 
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -25,7 +24,7 @@ public class Transactions {
 	@Column(name = "sender_id")
 	private Long senderId;
 	@Column(name = "reciever_id")
-	private Long recieverId;
+	private Long receiverId;
 	@Column(name = "txn_amount")
 	private Double txnAmount;
 	@Column(name = "sender_payment_method")
@@ -36,46 +35,46 @@ public class Transactions {
 	private String senderCardExpiry;
 	@Column(name = "sender_name_on_card")
 	private String senderNameOnCard;
-	@Column(name = "reciiver_payment_method")
-	private String recieverPaymentMethod;
-	@Column(name = "reciever_account_number")
-	private Long recieverAccountNumber;
-	@Column(name = "reciever_iban")
-	private String recieverIban;
+	@Column(name = "receiver_payment_method")
+	private String receiverPaymentMethod;
+	@Column(name = "receiver_account_number")
+	private Long receiverAccountNumber;
+	@Column(name = "receiver_iban")
+	private String receiverIban;
 	@Column(name = "txn_type")
 	private String txnType;
 	@Column(name = "fx_rate")
 	private Double fxRate;
 	@Column(name = "exchange_fee")
 	private Double exchangeFee;
-	@Column(name = "reciever_pay_out")
-	private Double recieverPayOut;
+	@Column(name = "receiver_pay_out")
+	private Double receiverPayOut;
 	@Column(name = "sender_currency")
 	private String senderCurrency;
-	@Column(name = "reciever_currency")
-	private String recieverCurrency;
-	@Column(name = "reciever_country_iso")
-	private String recieverCountryIso;
+	@Column(name = "receiver_currency")
+	private String receiverCurrency;
+	@Column(name = "receiver_country_iso")
+	private String receiverCountryIso;
 	@Column(name = "mtcn")
 	private Long mtcn;
 	@Column(name = "txn_status")
 	private String txnStatus;
 	@Column(name = "txn_date")
-	private Date txnDate;
+	private String txnDate;
 	@Column(name = "third_party_ref_id")
 	private String thirdPartyRefId;
 	@Column(name = "created_by")
 	private String createdBy;
 	@Column(name = "created_on")
-	private Date createdOn;
+	private String createdOn;
 	@Column(name = "modified_by")
 	private String modifiedBy;
 	@Column(name = "modified_on")
-	private Date modifiedOn;
+	private String modifiedOn;
 	@Column(name = "settlement_ref_id")
 	private String settlementRefId;
 	@Column(name = "txn_settled_on")
-	private Date txnSettledOn;
+	private String txnSettledOn;
 	@Column(name = "refund_reff_txn_id")
 	private String refundReffTxnId;
 	@Column(name = "remarks")
