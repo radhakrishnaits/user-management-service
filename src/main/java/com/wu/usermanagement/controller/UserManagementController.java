bc16a1149b3abdf11a374483eabb6038bd7016ff
nvsathyababu <nvsathyababu@altimetrik.com>
2023-08-25 23:17:21 +0530

    updated cross-orign code

--- src/main/java/com/wu/usermanagement/controller/UserManagementController.java
+++ src/main/java/com/wu/usermanagement/controller/UserManagementController.java
@@ -3,14 +3,7 @@ package com.wu.usermanagement.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.wu.usermanagement.model.AddBeneficiaryRequest;
 import com.wu.usermanagement.model.AddBeneficiaryResponse;
@@ -60,6 +53,7 @@ import lombok.extern.slf4j.Slf4j;
 @Tag(name = "user-management", description = "user-management APIs")
 @RestController
 @RequestMapping("/user-management/v1")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 
 /** The Constant log. */
 
